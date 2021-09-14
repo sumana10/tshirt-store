d0648df00a09cc23b5512815ca9fe10c0f57a46a
sumana10 <sumanaonlyme@gmail.com>
2021-09-15 03:40:59 +0530

    initial commit

---
+++ projfrontend/src/core/StripeCheckout.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { isAuthenticated } from "../auth/helper";
+import { cartEmpty, loadCart } from "./helper/cartHelper";
+import { Link } from "react-router-dom";
+import StripeCheckoutButton from "react-stripe-checkout";
+import { API } from "../backend";
+import { createOrder } from "./helper/orderHelper";
+
+const StripeCheckout = ({
+  products,
+  setReload = f => f,
+  reload = undefined
+}) => {
+  const [data, setData] = useState({
+    loading: false,
+    success: false,
+    error: "",
+    address: ""
+  });
+
+  const token = isAuthenticated() && isAuthenticated().token;
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+
+  const getFinalAmount = () => {
+    let amount = 0;
+    products.map(p => {
+      amount = amount + p.price;
+    });
+    return amount;
+  };
+
+  const makePayment = token => {
+    const body = {
+      token,
+      products
+    };
+    const headers = {
+      "Content-Type": "application/json"
+    };
+    return fetch(`${API}/stripepayment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        console.log(response);
+        //call further methods
+        const {status} = response;
+        console.log("STATUS", status);
+       // cartEmpty();
+      })
+      .catch(error => console.log(error));
+  };
+
+  const showStripeButton = () => {
+    return isAuthenticated() ? (
+      <StripeCheckoutButton
+        stripeKey="pk_test_51JT495SDrYAS3txwvUw20bPytHXNKtl0qq85OC0kFJUGUAdfQJ3zi7hTo0uVUj6Tx8DkINbF0OWdGbkZgw1vWQ68004LfiRi7K"
+        token={makePayment}
+        amount={getFinalAmount() * 100}
+        name="Buy Tshirts"
+        shippingAddress
+        billingAddress
+      >
+        <button className="btn btn-success">Pay with stripe</button>
+      </StripeCheckoutButton>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-warning">Signin</button>
+      </Link>
+    );
+  };
+
+  return (
+    <div>
+      <h3 className="text-white">Stripe Checkout {getFinalAmount()}</h3>
+      {showStripeButton()}
+    </div>
+  );
+};
+
+export default StripeCheckout;
---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link, Redirect } from "react-router-dom";
+
+import {signin, authenticate, isAuthenticated} from '../auth/helper';
+
+const Signin = () => {
+
+  const [values, setValues] = useState({
+    email: "sumana@gmail.com",
+    password: "12345",
+    error: "",
+    loading: false,
+    didRedirect: false
+
+  });
+  const {email, password, error, loading, didRedirect} = values;
+
+  const {user} = isAuthenticated();
+
+  const handleChange = name => event => {
+
+    setValues({...values, error: false, [name]: event.target.value})
+  }
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({...values, error: false, loading:true});
+    signin({ email, password })
+    .then(data =>{
+      if(data.error){
+        setValues({ ...values, error: data.error, loading: false })
+      }else{
+        authenticate(data, ()=>{
+        setValues({
+          ...values,
+          didRedirect: true
+        });
+      })
+      }
+    })
+    .catch(console.log("signin request failed"))
+  }
+  const performRedirect = () =>{
+    //TODO: do a redirection here
+    if(didRedirect){
+      if(user && user.role === 1){
+        return <Redirect to="/admin/dashboard"/>
+      }else{
+        return <Redirect to="/user/dashboard"/>
+      }
+    }
+    if(isAuthenticated()){
+      return <Redirect to="/"/>
+    }
+  }
+  const loadingMessage = () =>{
+    return( 
+      loading && (
+        <div className="alert alert-info">
+          <h2>Loading...</h2>
+        </div>
+      )
+     );
+   
+   }
+   const errorMessage = () =>{
+     return(
+     <div className="row">
+       <div className="col-md-6 offset-sm-3 text-left">
+         <div className="alert alert-danger"
+               style={{display: error ? "" : "none"}}>
+               {error}
+         </div>
+       </div>
+     </div>
+     );
+   
+   }
+
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="email" value={email}
+              onChange = {handleChange("email")} />
+            </div>
+
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" value={password}
+              onChange = {handleChange("password")} />
+            </div>
+            <div class="d-grid gap-2 py-2">
+              <button onClick={onSubmit} className="btn btn-success" type="button">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Sign In page" description="A page for user to sign in!">
+      {loadingMessage()}
+      {errorMessage()}
+      {signInForm()}
+      {performRedirect()}
+      <p className="text-white text-center">{JSON.stringify(values)}</p>
+    </Base>
+  );
+};
+
+export default Signin;
